affc40f8bae4a791e34619caa1776988c012f679
Trisha Ruz <2015081441@ust-ics.mygbiz.com>
2018-10-08 12:06:08 +0800

    hey

--- app/src/main/java/com/ruz/trisha/ruztrishamarielab5/MainActivity.java
+++ app/src/main/java/com/ruz/trisha/ruztrishamarielab5/MainActivity.java
@@ -4,58 +4,68 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btnActivity2;
-    Button Maps;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("4ITF ", "On create has executed....");
 
-        Intent i = new Intent(this, Service.class);
+        Intent i =  new Intent(this, Service.class);
         startService(i);
-
-        btnActivity2 = findViewById(R.id.btnActivity2);
-
-        btnActivity2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayActivity2(v);
-            }
-        });
-
-        Maps = findViewById(R.id.btnMaps);
-
-        Maps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayMap(v);
-            }
-        });
-
-
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("4ITF ", "On start has executed....");
+    }
 
-    public void displayActivity2(View v){
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("4ITF ", "On resume has executed....");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("4ITF ", "On pause has executed....");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("4ITF ", "On stop has executed....");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("4ITF ", "On Restart has executed....");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("4ITF ", "The object has been deallocated...");
+    }
+    public void display2(View v) {
         Intent i = new Intent(this, SecondActivity.class);
         startActivity(i);
-        finish();
     }
 
-
-    public void displayMap(View v){
+    public void process(View v) {
         Intent i = null, chooser = null;
-        if(v.getId() == R.id.btnMaps){
+        if (v.getId() == R.id.btnMaps) {
             i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse("http://maps.google.com/maps?q=loc:" + 14.5901252 + ","+ 121.036629));
-            chooser = Intent.createChooser(i, "Choose a Map app");
+            i.setData(Uri.parse("geo:14.586915, 120.981192"));
+            chooser = Intent.createChooser(i, "Select your map app");
+
             startActivity(chooser);
         }
     }
